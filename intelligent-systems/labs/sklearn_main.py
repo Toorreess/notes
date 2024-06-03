61410cfb01c20c757bfccbe991d40a25a4c156b0
JTorres <jtorres@exyt.es>
2024-06-03 15:23:44 +0200

    🚧wip: IS sklearn lab

--- intelligent-systems/labs/sklearn_main.py
+++ intelligent-systems/labs/sklearn_main.py
@@ -1,52 +1,117 @@
-from sklearn.tree import DecisionTreeClassifier, plot_tree
-from sklearn.model_selection import KFold, train_test_split
-from sklearn.metrics import accuracy_score
+import logging
 
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV, KFold, train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from ucimlrepo import fetch_ucirepo
 
-from ucimlrepo import fetch_ucirepo 
-  
-# fetch dataset 
-tic_tac_toe_endgame = fetch_ucirepo(id=101) 
-  
-# data (as pandas dataframes) 
+# fetch dataset
+tic_tac_toe_endgame = fetch_ucirepo(id=101)
+
+# data (as pandas DataFrames)
 X = pd.DataFrame(tic_tac_toe_endgame.data.features)
 y = pd.DataFrame(tic_tac_toe_endgame.data.targets)
 
 # Transform value 'o' to 0, 'x' to 1, 'b' to -1
-X = X.replace('o', 0)
-X = X.replace('x', 1)
-X = X.replace('b', -1)
+X = X.replace("o", 0)
+X = X.replace("x", 1)
+X = X.replace("b", -1)
 
 # Transform value 'positive' to 1, 'negative' to 0
-y = y.replace('positive', 1)
-y = y.replace('negative', 0)
+y = y.replace("positive", 1)
+y = y.replace("negative", 0)
 
 
 # Exercise 1: Classify the data with a decision tree classifier. You must find out the best hyperparameters and plot the best decision tree.
-# * Split the dataset into training and testing sets
-kf = KFold(n_splits=10, shuffle=True, random_state=42)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# * Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
-# * Create the model
-model = DecisionTreeClassifier(max_depth=10)
+kf = KFold(n_splits=10)
+kf.get_n_splits(X)
 
-# * Train the model
-model.fit(X_train, y_train)
+# * Define the hyperparameters to search
+param_grid = {
+    "max_depth": [i for i in range(10)],
+    "min_samples_split": [i for i in range(10)],
+    "min_samples_leaf": [i for i in range(10)],
+}
 
-# * Evaluate model
-prediction = model.predict(X_test)
+# * Perform grid search to find the best hyperparameters
+grid_search = GridSearchCV(DecisionTreeClassifier(), param_grid, cv=10)
+grid_search.fit(X_train, y_train)
 
-accuracy = accuracy_score(y_true=y_test, y_pred=prediction)
+# * Get the best hyperparameters
+best_params = grid_search.best_params_
 
+logging.info(f" Best params: {best_params}")
 
-# * Plot the decision tree
-plot_tree(model, feature_names=X.columns, class_names=tic_tac_toe_endgame.data.target_names, filled=True)
-# plt.title("Decision Tree", size=20, color='red')
+# * Train the decision tree classifier with the best hyperparameters
+best_dtc = DecisionTreeClassifier(**best_params)
+best_dtc.fit(X_train, y_train)
+
+# * Predict the test set labels
+y_pred = best_dtc.predict(X_test)
+
+# * Calculate the accuracy of the classifier
+accuracy = accuracy_score(y_test, y_pred)
+logging.info(f" accuracy:{accuracy}")
+
+# * Plot the best decision tree
+plt.figure(figsize=(15, 15))
+plot_tree(
+    best_dtc,
+    max_depth=3,
+    label="root",
+    feature_names=X.columns,
+    class_names=["negative", "positive"],
+    filled=True,
+    rounded=True,
+    fontsize=7,
+)
+
 plt.show()
 
-# Excercise 2 : Classify the data with a multilayer perceptron classifier. You must find out the best architecture (number of layers, number of hidden layers, activation functions) and the best hyperparameters
 
+# Exercise 2 : Classify the data with a multilayer perceptron classifier. You must find out the best architecture (number of layers, number of hidden layers, activation functions) and the best hyperparameters
+# * Define the hyperparameters to search
+param_grid = {
+    "hidden_layer_sizes": [(i,) for i in range(20)],
+    "activation": ["relu", "tanh", "logistic"],
+    "alpha": [0.01],
+}
+
+# Scale the data
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Flatten y_train before fitting the model
+y_train = np.ravel(y_train)
+
+# Perform grid search to find the best hyperparameters
+grid_search = GridSearchCV(
+    MLPClassifier(max_iter=2000), param_grid, cv=10, scoring="accuracy"
+)
+grid_search.fit(X_train_scaled, y_train)
+
+# Get the best hyperparameters
+best_params = grid_search.best_params_
+
+# Train the MLP classifier with the best hyperparameters
+best_mlp = MLPClassifier(**best_params)
+best_mlp.fit(X_train_scaled, y_train)
+
+# Predict the test set labels
+y_pred = best_mlp.predict(X_test_scaled)
+
+# Calculate the accuracy of the classifier
+accuracy = accuracy_score(y_test, y_pred)
+logging.info(f" accuracy:{accuracy}")
