6f56cd8fba09a5572c89e198b0f72eb2a58d7a51
J.Torres <jostorpos@uma.es>
2024-09-18 21:03:04 +0200

    :sparkles: feat: 2024-09-18

--- Tª de Dominios/Ejercicios Programación/1-1.go
+++ Tª de Dominios/Ejercicios Programación/1-1.go
@@ -5,13 +5,43 @@ type Pair struct {
 	Second any
 }
 
-func isReflexive(P []string, R []interface{}) bool {
-	for _, x := range P {
-		p := Pair{First: x, Second: x}
-
+func isReflexive(p Pair, R []Pair) bool {
+	for _, r := range R {
+		if p == r {
+			return true
+		}
 	}
-	return true
+	return false
 }
 
-func isTransitive()    {}
-func isAntiSymmetric() {}
+func isTransitive(p Pair, R []Pair) {
+
+}
+
+func isAntisymmetric(p Pair, R []Pair) bool {
+	firstCheck := false
+	secondCheck := false
+
+	pAux := Pair{
+		First:  p.Second,
+		Second: p.First,
+	}
+
+	for _, r := range R {
+		if p == r {
+			firstCheck = true
+		}
+		if pAux == r {
+			secondCheck = true
+		}
+		if firstCheck && secondCheck {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	// Conjunto P de números o caracteres
+	// Relación R, dado mediante parejas de elementos
+}
